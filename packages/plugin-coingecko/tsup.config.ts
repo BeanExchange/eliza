ba2cb7173541ebd6aecb65df1c61ce15c8515053
aaronng102 <aaron.deepblock@gmail.com>
2024-12-31 17:06:50 +0700

    fix

--- packages/plugin-coingecko/tsup.config.ts
+++ packages/plugin-coingecko/tsup.config.ts
@@ -7,5 +7,23 @@ export default defineConfig({
     clean: true,
     format: ["esm"], // Ensure you're targeting CommonJS
     external: [
+        "dotenv", // Externalize dotenv to prevent bundling
+        "fs", // Externalize fs to use Node.js built-in module
+        "path", // Externalize other built-ins if necessary
+        "@reflink/reflink",
+        "@node-llama-cpp",
+        "https",
+        "http",
+        "agentkeepalive",
+        "safe-buffer",
+        "base-x",
+        "bs58",
+        "borsh",
+        "@solana/buffer-layout",
+        "stream",
+        "buffer",
+        "querystring",
+        "amqplib",
+        // Add other modules you want to externalize
     ],
 });
