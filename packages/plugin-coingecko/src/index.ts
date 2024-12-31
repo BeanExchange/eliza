a8d3e5ee9397b9100d65cf3df5ec9e1bb7b966b9
aaronng102 <aaron.deepblock@gmail.com>
2024-12-31 18:12:05 +0700

    fix

--- packages/plugin-coingecko/src/index.ts
+++ packages/plugin-coingecko/src/index.ts
@@ -2,10 +2,13 @@ import { Plugin } from '@elizaos/core';
 import { getCoinData } from './actions/getCoinData';
 
 const CoinGeckoPlugin: Plugin = {
+  description: "Pull marketdata from coinGecko",
   name: 'coingecko',
-  actions: {
-    getCoinData,
-  },
+  actions: [getCoinData],
+  providers: [],
+  evaluators: [],
+  services: [],
+  clients: []
 };
 
 export default CoinGeckoPlugin;
